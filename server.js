0cca2ff42e73a66a8fec150260236a5bf1ca8056
yotlaire <gyotlaire@gmail.com>
2024-03-29 19:41:55 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World from Node.js backend!');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
