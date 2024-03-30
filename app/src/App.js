0beba9dcffd5001dab3ba0476278f8d24c8ae757
yotlaire <gyotlaire@gmail.com>
2024-03-29 20:10:06 -0400

    chnage from client to app

---
+++ app/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Hello World.... I am alive111!</h1>
+        </div>
+    );
+}
+
+export default App;
